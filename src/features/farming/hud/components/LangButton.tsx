e7f6fb3ce103432ea16f432ccc2fc83dba101756
GokuCrypto <8019835@qq.com>
2022-08-16 13:33:50 +0800

    # addd

--- src/features/farming/hud/components/LangButton.tsx
+++ src/features/farming/hud/components/LangButton.tsx
@@ -1,41 +1,46 @@
-import React,{ useCallback } from 'react';
-import { useTranslation } from 'react-i18next'; 
+import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
 import { InnerPanel } from "components/ui/Panel";
-import lang from 'assets/icons/lang.svg';
-import { withDefaultProps } from './Rxt';
+import lang from "assets/icons/lang.svg";
+import { withDefaultProps } from "./Rxt";
 const defaultProps = {};
 type CptProps = {} & typeof defaultProps;
 const LangButtonCpt: React.FC = () => {
   const { i18n } = useTranslation();
 
- 
-
- 
-
   const languageClick = useCallback(async (e) => {
     console.log(e.target.value);
-    if(e.target.value ==='en'){
-      await i18n.changeLanguage('zh');
-      localStorage.setItem('swapLanguage', 'zh');
-      e.target.value='zh';
-    }else{
-      await i18n.changeLanguage('en');
-      localStorage.setItem('swapLanguage', 'en');
-      e.target.value='en';
+    if (e.target.value === "en") {
+      await i18n.changeLanguage("zh");
+      localStorage.setItem("swapLanguage", "zh");
+      e.target.value = "zh";
+    } else {
+      await i18n.changeLanguage("en");
+      localStorage.setItem("swapLanguage", "en");
+      e.target.value = "en";
     }
-   
-    
-    console.log('defaultValue',e.target.value);
-    
+
+    console.log("defaultValue", e.target.value);
   }, []);
 
   return (
-    <InnerPanel className={'fixed top-2 right-20 z-50 flex items-center shadow-lg  '}>
-      <select onClick={languageClick} className="language-button-select" defaultValue={i18n.language || 'en'} >
-        <option value="en">English</option>
-        <option value="zh">简体中文</option>
+    <InnerPanel
+      className={"fixed  top-2 right-20 z-50 flex items-center shadow-lg  "}
+    >
+      <select
+        onClick={languageClick}
+        className="language-button-select"
+        defaultValue={i18n.language || "en"}
+      >
+        <option value="en">En</option>
+        <option value="zh">CN</option>
       </select>
-      <img src={lang} style={{ width: '25px' }} alt="LangIcon" />
+      <img
+        src={lang}
+        onClick={languageClick}
+        style={{ width: "25px" }}
+        alt="LangIcon"
+      />
     </InnerPanel>
   );
 };
--- src/features/game/WolfTown.tsx
+++ src/features/game/WolfTown.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import ScrollContainer from "react-indiana-drag-scroll";
 
-import background from "assets/land/background.jpg";
+import background from "assets/land/scroller2.png";
 
 import { Game } from "./Game";
 import { GameProvider } from "./GameProvider";
